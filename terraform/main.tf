e77258d6fd4861918496c0bc71776b58e27c23c5
Colton McCurdy <mccurdyc22@gmail.com>
2020-02-28 09:40:21 -0500

    seems to be working. rough draft
    Signed-off-by: Colton McCurdy <mccurdyc22@gmail.com>

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,12 @@
-resource "google_billing_account_iam_binding" "binding" {
+resource "google_billing_account_iam_member" "binding" {
   billing_account_id = "01A464-869E60-AC80F8"
-  role               = "roles/billing.viewer"
-
-  members = [
-    "user:mccurdyc22@gmail.com",
-  ]
-}
-
-data "google_billing_account" "mccurdyc_dot_dev_billing_acct" {
-  billing_account = google_billing_account_iam_binding.binding.id
-  open            = true
+  role               = "roles/billing.admin"
+  member = "user:mccurdyc22@gmail.com"
 }
 
 resource "google_project" "mccurdyc_dot_dev" {
   name       = "mccurdyc-dot-dev"
   project_id = "daring-octane-268913"
 
-  billing_account = data.google_billing_account.mccurdyc_dot_dev_billing_acct.id
+  billing_account = google_billing_account_iam_member.binding.billing_account_id
 }
--- terraform/storage-bucket.tf
+++ terraform/storage-bucket.tf
@@ -1,4 +1,19 @@
 resource "google_storage_bucket" "mccurdyc-dot-dev-store" {
-  name    = "mccurdyc.dev"
-  project = "daring-octane-268913"
+  name          = "mccurdyc-dot-dev-store"
+  project       = "daring-octane-268913"
+  force_destroy = true
+  storage_class = "REGIONAL"
+  location      = "US-EAST1"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
 }
+
+resource "google_storage_default_object_access_control" "public_bucket_access" {
+  bucket = google_storage_bucket.mccurdyc-dot-dev-store.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
